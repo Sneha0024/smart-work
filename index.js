91dae234ff58a311f1407825b6157e9a32568fb9
Sneha0024 <77921781+Sneha0024@users.noreply.github.com>
2021-02-17 21:51:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var menubtn = document.getElementById("menubtn");
+var sidenav = document.getElementById("sidenav");
+
+sidenav.style.right = "-250px";
+menubtn.onclick = function(){
+    if(sidenav.style.right =="-250px"){
+        sidenav.right.style = "0";
+    }
+    else{
+        sidenav.style.right = "-250px";
+    }
+    
+}
